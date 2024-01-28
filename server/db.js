979fcb09d24ec8969f50e8e248827b900837b8de
saahask <saahask@gmail.com>
2024-01-28 10:45:52 -0800

    Fixed bug with Docker shutting down Postgre server

--- server/db.js
+++ server/db.js
@@ -4,28 +4,28 @@ const pool = new Pool({
   user: "postgres",
   password: "test",
   host: "db",
-  port: 5432
+  port: 5432,
 });
 
 // Example query execution
-pool.query('CREATE DATABASE perntodo;', (err, result) => {
+pool.query("CREATE DATABASE perntodo;", (err, result) => {
   if (err) {
-    console.error('Error making database', err);
+    console.error("Error making database", err);
   } else {
-    pool.query('USE perntodo;');
-    console.log('Table created successfully');
+    pool.query("USE perntodo");
+    console.log("Table created successfully");
   }
 });
 
-pool.query('CREATE TABLE todo(todo_id SERIAL PRIMARY KEY, description VARCHAR(255));', (err, result) => {
-  if (err) {
-    console.error('Error making table', err);
-  } else {
-    console.log('Table created successfully');
+pool.query(
+  "CREATE TABLE todo(todo_id SERIAL PRIMARY KEY, description VARCHAR(255));",
+  (err, result) => {
+    if (err) {
+      console.error("Error making table", err);
+    } else {
+      console.log("Table created successfully");
+    }
   }
-});
-
+);
 
 module.exports = pool;
-
-
